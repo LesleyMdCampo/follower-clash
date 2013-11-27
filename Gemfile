8b067c5b7a18a04696f43e66de609f6c4e297473
LesleyMdCampo <lesliz32@gmail.com>
2013-11-27 10:49:25 -0600

    Add recent tweet, name to client.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
-gem 'foreman'
 
+gem 'foreman'
 gem 'twitter'
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -1,24 +1,40 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'twitter'
+require 'foreman'
 
 module UserComparer
   class User
   	attr_reader :username
   	def initialize(username)
   		@username = username
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+      end
   	end
 
   	def followers
-  	  client = Twitter::REST::Client.new do |config|
-  	    config.consumer_key        = ENV['CONSUMER_KEY']
-  	    config.consumer_secret     = ENV['CONSUMER_SECRET']
-  	    config.access_token         = ENV['OAUTH_TOKEN']
-  	    config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
-  	  end
-
-  	  client.user(@username).followers_count
+  	  @client.user(@username).followers_count
   	end
+
+    def friends
+      @client.user(@username).friends_count
+    end
+
+    def tweets
+      @client.user(@username).statuses_count
+    end
+
+    def last_status
+      @client.user(@username).status.text
+    end
+
+    def name
+      @client.user(@username).name
+    end
 
   end
 
@@ -38,6 +54,28 @@ module UserComparer
   			return "#{@user1.username} and #{@user2.username} have the same number of followers!  Who knew!?"
   		end
   	end
+
+    def comp_friends
+      # returns user with more friends, or both if even
+      if @user1.friends > @user2.friends
+        return @user1.username
+      elsif @user1.friends < @user2.friends
+        return @user2.username
+      else
+        return "#{@user1.username} and #{@user2.username} have the same number of friends!  Who knew!?"
+      end
+    end
+
+    def comp_tweets
+      # returns user with more tweeta, or both if even
+      if @user1.tweets > @user2.tweets
+        return @user1.username
+      elsif @user1.tweets < @user2.tweets
+        return @user2.username
+      else
+        return "#{@user1.username} and #{@user2.username} have the same number of tweets!  Who knew!?"
+      end
+    end
 
   end
 end
